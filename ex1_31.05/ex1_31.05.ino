fd0163ea04c0e62d62a08c537533d8b9876b370d
Gustavo Marques de Lanna <99662123+GustavoLanna11@users.noreply.github.com>
2022-06-10 10:08:44 -0300

    Versão 1.0

---
+++ ex1_31.05/ex1_31.05.ino
@@ -0,0 +1,27 @@
+int led=10;
+int led2=9;
+int led3=8;
+char tecla;
+
+void setup() {
+   pinMode(led, OUTPUT);
+   pinMode(led2, OUTPUT);
+   pinMode(led3, OUTPUT);
+   Serial.begin(9600);
+   
+   
+}
+
+void loop() {
+  tecla=Serial.read();
+
+  if (tecla=='L')
+  {
+      digitalWrite(led,HIGH);
+      digitalWrite(led2,HIGH);
+
+      Serial.write(tecla);
+      delay(1000);
+  }
+
+}
---
+++ ex3_31.05/ex3_31.05.ino
@@ -0,0 +1,94 @@
+int led=10;
+int led2=9;
+int led3=8;
+char tecla;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  tecla=Serial.read();
+
+  if(tecla=='K')
+  {
+    digitalWrite(led, HIGH);
+    delay(200);
+    digitalWrite(led2, HIGH);
+    delay(200);
+    digitalWrite(led3, HIGH);
+    delay(200);
+
+    Serial.write(tecla);
+    delay(1000);
+  }
+
+  else if(tecla=='P')
+  {
+
+    digitalWrite(led, HIGH);
+    delay(200);
+    digitalWrite(led2, HIGH);
+    delay(200);
+    digitalWrite(led3, HIGH);
+    delay(200);
+
+    digitalWrite(led, LOW);
+    delay(200);
+    digitalWrite(led2, LOW);
+    delay(200);
+    digitalWrite(led3, LOW);
+    delay(200);
+
+
+
+
+
+    digitalWrite(led, HIGH);
+    delay(200);
+    digitalWrite(led2, HIGH);
+    delay(200);
+    digitalWrite(led3, HIGH);
+    delay(200);
+
+    digitalWrite(led, LOW);
+    delay(200);
+    digitalWrite(led2, LOW);
+    delay(200);
+    digitalWrite(led3, LOW);
+    delay(200);
+
+
+
+
+
+    digitalWrite(led, HIGH);
+    delay(200);
+    digitalWrite(led2, HIGH);
+    delay(200);
+    digitalWrite(led3, HIGH);
+    delay(200);
+
+    digitalWrite(led, LOW);
+    delay(200);
+    digitalWrite(led2, LOW);
+    delay(200);
+    digitalWrite(led3, LOW);
+    delay(200);
+
+
+
+    digitalWrite(led, HIGH);
+    delay(200);
+    digitalWrite(led2, HIGH);
+    delay(200);
+    digitalWrite(led3, HIGH);
+    delay(200);
+     
+ 
+  }
+}
